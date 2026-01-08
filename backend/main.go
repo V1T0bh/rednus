2c82ef18309c3e8f01bdf2f48d95d6f52122d38a
V1T0bh <vitobenedicthalim@gmail.com>
2026-01-08 15:39:05 +0700

    add routers to main

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/V1T0bh/rednus/backend/controllers"
 	"github.com/V1T0bh/rednus/backend/initalizers"
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +13,18 @@ func init() {
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "Post-Created",
+	// 	})
+	// })
+
+	// Post routes
+	router.POST("/posts", controllers.PostsCreate)
+	router.GET("/posts", controllers.PostsAll)
+	router.GET("/posts/:id", controllers.PostsIndex)
+	router.PUT("/posts/:id", controllers.PostsUpdate)
+	router.DELETE("/posts/:id", controllers.PostsDelete)
+
 	router.Run() // listens on 0.0.0.0:8080 by default
 }
