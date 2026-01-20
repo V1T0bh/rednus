48549ec05a758fb56e85d4511cd2f70d126fabfe
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2026-01-20 22:08:41 +0800

    fix initializers typo in migrate

--- backend/migrate/migrate.go
+++ backend/migrate/migrate.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/V1T0bh/rednus/backend/initalizers"
+	"github.com/V1T0bh/rednus/backend/initializers"
 	"github.com/V1T0bh/rednus/backend/models"
 )
 
 func init() {
-	initalizers.LoadEnv()
-	initalizers.ConnectToDB()
+	initializers.LoadEnv()
+	initializers.ConnectToDB()
 }
 
 func main() {
-	initalizers.DB.AutoMigrate(&models.User{}, &models.Post{}, &models.Topic{})
+	initializers.DB.AutoMigrate(&models.User{}, &models.Post{}, &models.Topic{})
 }
