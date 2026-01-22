39e20b0a47e8ec59651e3c4f2b69959d212b0731
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2026-01-22 14:54:34 +0800

    implement skeleton sign in page

--- frontend/app/sign-in/page.tsx
+++ frontend/app/sign-in/page.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
+import { Title } from "@/components/title";
+import { Subtitle } from "@/components/subtitle";
 
 export default function SignIn() {
-
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-xl font-bold text-zinc-800 dark:text-zinc-200">
-        This is a sign in page.
-      </h1>
+    return (
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50">
+      <Subtitle>
+        Sign In
+      </Subtitle>
+      <div className="flex flex-col justify-center mb-5">
+        <input type="text" placeholder="Username" className="border-2 border-slate-400 rounded-lg mb-4 w-110 text-xl p-4"/>
+      </div>
+      <Link href="/sign-in">
+      <button className="cursor-pointer text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-2xl w-110 py-5 text-center leading-5">
+          SIGN IN
+        </button>
+      </Link>
     </div>
-  );
+    )
 }
