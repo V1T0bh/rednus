72d0bf4f459abf0490ac1f1e5c80ad1d52595911
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2026-01-25 01:10:30 +0800

    fix styling profile page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -2,7 +2,7 @@ import { Title } from "@/components/title";
 
 export default function ProfilePage() {
   return (
-    <div className="flex flex-col min-h-[calc(100vh-200px)] items-start justify-center pt-10 px-50">
+    <div className="flex flex-col fill-screen items-start justify-center pt-10 px-50">
         <Title>Username: {"User123"}</Title>
         <button 
             className="cursor-pointer text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-2xl px-50 py-5 text-center leading-5"
