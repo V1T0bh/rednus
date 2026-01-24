d0c25b6ab036705e604aa3de295eb1abf3520a74
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2026-01-24 23:52:26 +0800

    implement simple profile page

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,13 @@
+
+export default function ProfilePage() {
+  return (
+    <div>
+        <p>Username: {"User123"}</p>
+        <button 
+            className="cursor-pointer text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-2xl px-50 py-5 text-center leading-5"
+            >
+          SIGN OUT
+        </button>
+    </div>
+  );
+}
