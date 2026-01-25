50c2ae37d1d28e226570fc9fcc7a9564f5bef2de
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2026-01-25 21:28:26 +0800

    fix routes

--- backend/routes.go
+++ backend/routes.go
@@ -1,22 +1,47 @@
 package main
 
-import "github.com/V1T0bh/rednus/backend/controllers"
+import (
+	"github.com/V1T0bh/rednus/backend/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRoutes() {
-	// Define your routes here
-	router.POST("/posts", controllers.PostsCreate)
-	router.POST("topics/:topic_id/posts/", controllers.PostsCreateinTopic)
-	router.GET("/posts", controllers.PostsAll)
-	router.GET("/topics/:topic_id/posts", controllers.PostsAllinTopic)
-	router.GET("/posts/:id", controllers.PostsIndex)
-	router.PUT("/posts/:id", controllers.PostsUpdate)
-	router.DELETE("/posts/:id", controllers.PostsDelete)
+	// CORS middleware
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Username")
 
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
+	// Topics routes
 	router.POST("/topics", controllers.TopicsCreate)
 	router.GET("/topics", controllers.TopicsAll)
-	router.PUT("/topics/:id", controllers.TopicsUpdate)
-	router.DELETE("/topics/:id", controllers.TopicsDelete)
+	router.GET("/topics/:topic_id", controllers.TopicsIndex)
+	router.PUT("/topics/:topic_id", controllers.TopicsUpdate)
+	router.DELETE("/topics/:topic_id", controllers.TopicsDelete)
 
+	// Posts routes
+	router.POST("/posts", controllers.PostsCreate)
+	router.POST("/topics/:topic_id/posts/", controllers.PostsCreateinTopic)
+	router.GET("/posts", controllers.PostsAll)
+	router.GET("/topics/:topic_id/posts", controllers.PostsAllinTopic)
+	router.GET("/posts/:post_id", controllers.PostsIndex)
+	router.PUT("/posts/:post_id", controllers.PostsUpdate)
+	router.DELETE("/posts/:post_id", controllers.PostsDelete)
+
+	// Comments routes
+	router.POST("/posts/:post_id/comments", controllers.CommentsCreate)
+	router.GET("/posts/:post_id/comments", controllers.CommentsAllInPost)
+	router.DELETE("/comments/:id", controllers.CommentsDelete)
+
+	// User routes
 	router.POST("/signin", controllers.SignIn)
 	router.GET("/users", controllers.GetUsers)
 }
