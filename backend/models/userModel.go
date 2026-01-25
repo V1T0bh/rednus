21122bf6acd597114569bb92aaa0f7d40f6bddf6
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2026-01-25 22:41:42 +0800

    add password to user

--- backend/models/userModel.go
+++ backend/models/userModel.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name  string `gorm:"unique"`
-	Admin bool   `gorm:"default:false"`
+	Name     string `gorm:"unique"`
+	Password string `json:"-"` // Never expose password in JSON responses
+	Admin    bool   `gorm:"default:false"`
 }
